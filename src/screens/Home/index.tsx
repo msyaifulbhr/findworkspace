d24aa68e32878316447ecb6ed11a8ed72b55ac09
msyaifulbhr <syaifulyy@gmail.com>
2024-01-20 01:54:42 +0700

    add asset and global style

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -2,6 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { useNavigation } from '@react-navigation/native';
 import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { colors } from '../../../assets/styles/Colors';
 
 export default function Home() {
   const navigation = useNavigation();
@@ -22,7 +23,7 @@ export default function Home() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#121212',
+    backgroundColor: colors.primary,
     alignItems: 'center',
     justifyContent: 'center',
   },
