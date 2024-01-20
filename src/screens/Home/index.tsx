32f50a33d7091819aef4e14074480c75d4dd1121
msyaifulbhr <syaifulyy@gmail.com>
2024-01-20 23:33:07 +0700

    Slicing Header in HomeScreen

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,20 +1,54 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { Image } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, ScrollView } from 'react-native';
 import { colors } from '../../../assets/styles/Colors';
+import { Gs } from '../../../assets/styles/GlobalStyle';
 
 export default function Home() {
   const navigation = useNavigation();
+
+  const renderHeader = () => {
+    return (
+      <View style={styles.headerContainer}>
+        <View style={Gs.flexRow}>
+          <Image 
+          source={require('../../../assets/images/profile_1.png')}
+          style={styles.profileContainer}
+          />
+          <View>
+            <Text style={Gs.textBlack}>Hi, Shayna</Text>
+            <Text style={[Gs.font700, Gs.textBlack]}>@shaynawork</Text>
+          </View>
+        </View>
+        <View style={Gs.flexRow}>
+          <Image 
+          source={require('../../../assets/icons/gift.png')}
+          style={styles.iconContainer}
+          />
+          <Image 
+          source={require('../../../assets/icons/notification.png')}
+          style={styles.iconContainer}
+          />
+
+        </View>
+      </View>
+    )
+      
+    
+  };
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Home</Text>
-      <TouchableOpacity 
-        onPress={() => {
-          navigation.navigate('Details');
-        }}>
-        <Text style={styles.text}>Redirect to Details</Text>
-      </TouchableOpacity>
+      <View>
+        {renderHeader()}
+        {/* {renderSearch()} */}
+        <ScrollView>
+          {/* {renderPopularSection()} */}
+          {/* {renderNewsworthy()} */}
+        </ScrollView>
+      </View>
+      {/* <ButtomNav /> */}
       <StatusBar style="auto" />
     </View>
   );
@@ -23,12 +57,24 @@ export default function Home() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: colors.primary,
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: colors.greyLight,
+    paddingTop: 24,
   },
-  text: {
-    color: '#fff',
+  headerContainer: {
+    ...Gs.flexRow,
+    ...Gs.itemsCenter,
+    ...Gs.justifyBetween,
+    padding: 24,
+  },
+  profileContainer: {
+    width: 50,
+    height: 50,
+    marginRight: 10,
+  },
+  iconContainer: {
+    width: 24,
+    height: 24,
+    marginLeft: 10,
   }
 });
 
